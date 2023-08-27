ad8f6f6d129e8f847d9db4a826fec35611d25292
4eh711 <Cheh711@gmail.com>
2023-08-27 21:43:09 +0300

    Less_5 Ex7.модуль __builtins__.

--- sys_mod.py
+++ sys_mod.py
@@ -1,10 +1,2 @@
-import sys
-import requests
-
-print(sys.executable)
-print(sys.version)
-print(sys.platform)
-print(sys.argv)
-
-for module_name, module_path in sys.modules.items():
-    print(module_name, module_path)
+for _ in dir(__builtins__):
+    print(_)
